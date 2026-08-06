3f894759e5e3e5f14d6c01396788a74bd1ba27fe
kritesh.j <kritesh.jaiswal@cyberevolve.com>
2026-08-06 23:04:14 +0530

    Added new Changes and login functionality in Production server.

--- src/main/java/com/aidocqa/config/SecurityConfig.java
+++ src/main/java/com/aidocqa/config/SecurityConfig.java
@@ -58,7 +58,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:5173", "http://localhost:3000", "http://localhost:8080"));
+        configuration.setAllowedOrigins(List.of("http://localhost:5173", "http://localhost:3000", "http://localhost:8080",  "https://ai-doc-theta.vercel.app"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
